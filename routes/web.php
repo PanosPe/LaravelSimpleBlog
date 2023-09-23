1c76df6814659c89f835047b514ec3d2b4afeeb5
PanosPe <panospe2001@gmail.com>
2023-09-23 14:41:49 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//User related  routes
+Route::get('/', [UserController::class, "ShowCorrectHomepage"]);
+
+Route::post('/register', [UserController::class, 'register']);
+
+Route::post('/login', [UserController::class, 'login']);
+
+Route::post('/logout', [UserController::class, 'logout']);
+
+
+//Blog related routes
+Route::get('/create-post', [PostController::class, 'showCreateForm']);
+
+Route::post('/create-post', [PostController::class, 'storeNewPost']);
+
+Route::get('/post/{post}', [PostController::class, 'showSinglePost']);
+
